60b47b490b63d03acabc82ababe9875e333ce56c
Hunter Krieger <hunterk31@hotmail.com>
2018-10-11 15:44:51 -0400

    upload data

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lefdbeqiysaorl:1f8408a1445e1b482ba5dd0900e401202269d6ec7f6e4168871e0b6a1fb60fa1@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d8n1smr8bnouet'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bicqwucdvbgpau:94a09da7ef0a9c9e512130cd5efc0ab9170d55d48fc594160a76dd3c9738e303@ec2-54-225-97-112.compute-1.amazonaws.com:5432/df8l77hauvn3gt'
 
 app.config['SECRET_KEY'] = '7"#~L?y78hWq%~NV'
 
