3be759441c81693072df5f9a3623c50af8fa5c30
Hunter Krieger <hunterk31@hotmail.com>
2018-10-28 07:44:27 -0400

    change cred

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bicqwucdvbgpau:94a09da7ef0a9c9e512130cd5efc0ab9170d55d48fc594160a76dd3c9738e303@ec2-54-225-97-112.compute-1.amazonaws.com:5432/df8l77hauvn3gt'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wjkuybwrjllzoi:b241032ce098ab7709929f39be0ab3ea831693657bfc573e5c982df15720a306@ec2-75-101-138-26.compute-1.amazonaws.com:5432/d2t3epaqj8anqe'
 
 app.config['SECRET_KEY'] = '7"#~L?y78hWq%~NV'
 
