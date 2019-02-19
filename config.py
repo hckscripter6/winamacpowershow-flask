d55ac66c0895ad0f56c3ade8562faa7dda802e27
Hunter Krieger <hunterk31@hotmail.com>
2019-02-18 22:21:21 -0500

    change config

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wjkuybwrjllzoi:b241032ce098ab7709929f39be0ab3ea831693657bfc573e5c982df15720a306@ec2-75-101-138-26.compute-1.amazonaws.com:5432/d2t3epaqj8anqe'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
-app.config['SECRET_KEY'] = '7"#~L?y78hWq%~NV'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
